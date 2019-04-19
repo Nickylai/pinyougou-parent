492f9cc6ad6c759975eaa9bd8dbf4bd04cf0fb5b
Nickylai <497000143@qq.com>
2019-04-20 00:26:27 +0800

    商家入驻功能实现账号注册，商家信息录入，前后端联动跑通；发现服务器响应缓慢，有较长延迟，需要排查解决

--- pinyougou-shop-web/src/main/webapp/js/controller/sellerController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/sellerController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('sellerController' ,function($scope,$controller   ,sellerService){	
+app.controller('sellerController' ,function($scope,$controller,sellerService){
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -31,23 +31,17 @@ app.controller('sellerController' ,function($scope,$controller   ,sellerService)
 		);				
 	}
 	
-	//保存 
-	$scope.save=function(){				
-		var serviceObject;//服务层对象  				
-		if($scope.entity.id!=null){//如果有ID
-			serviceObject=sellerService.update( $scope.entity ); //修改  
-		}else{
-			serviceObject=sellerService.add( $scope.entity  );//增加 
-		}				
-		serviceObject.success(
+	//新增
+	$scope.add=function(){
+        sellerService.add($scope.entity).success(
 			function(response){
 				if(response.success){
-					//重新查询 
-		        	$scope.reloadList();//重新加载
+					//成功跳转到登录页面
+		        	location.href="shoplogin.html";
 				}else{
 					alert(response.message);
 				}
-			}		
+			}
 		);				
 	}
 	

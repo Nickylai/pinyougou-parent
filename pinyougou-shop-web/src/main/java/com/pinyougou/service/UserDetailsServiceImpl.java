2c85c33844e1e557ba08659985907054a970638d
Nickylai <497000143@qq.com>
2019-04-20 19:33:55 +0800

    完成认证类的服务远程调用，通过比对后台数据库完成商户登录

--- pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.service;
 
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -19,13 +21,37 @@ import java.util.List;
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("经过了UserDetailsServiceImpl");
 
-        List<GrantedAuthority> grantAuths=new ArrayList<>();
+        List<GrantedAuthority> grantAuths = new ArrayList<>();
         grantAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
-        return new User(username,"123456",grantAuths);
-    }
 
+        TbSeller seller = sellerService.findOne(username);
+//        System.out.println(seller.getName()+seller.getPassword());
+
+        //status为1，商户为审核通过状态
+        String status = "1";
+        if (seller != null) {
+            if (seller.getStatus().equals(status)) {
+                return new User(username, seller.getPassword(), grantAuths);
+            } else {
+                return null;
+            }
+
+        } else {
+            return null;
+        }
+
+
+    }
 }

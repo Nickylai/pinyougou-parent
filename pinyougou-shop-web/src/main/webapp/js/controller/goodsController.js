3ecfcda17a40964b094780bd9c2e92a9577b1369
Nickylai <497000143@qq.com>
2019-04-25 23:43:31 +0800

    商户后端,查询商品列表：显示分类的名字，先查询所有分类，在根据id查询名字

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -232,5 +232,17 @@ app.controller('goodsController' ,function($scope,$controller,goodsService,uploa
 
     $scope.status = ['未审核','已审核','审核未通过','已通过'];
 
+    $scope.itemCatList = [];
+
+	$scope.findItemCatList=function () {
+        itemCatService.findAll().success(
+            function (response) {
+                for (var i = 0; i < response.length; i++) {
+                    $scope.itemCatList[response[i].id] = response[i].name;
+                }
+            }
+        );
+
+    }
 
 });	

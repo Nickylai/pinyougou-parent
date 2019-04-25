f033997686c5ee099aaf10f17aa532bd45c67f4f
Nickylai <497000143@qq.com>
2019-04-26 00:13:34 +0800

    商户后端,商品修改：前端代码查询商品基本信息

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller,goodsService,uploadService,itemCatService,typeTemplateService){
+app.controller('goodsController' ,function($scope,$location,$controller,goodsService,uploadService,itemCatService,typeTemplateService){
 	
 	$controller('baseController',{$scope:$scope});//继承
     $scope.entity = {goods:'', goodsDesc: {itemImages: [], specificationItems: []},itemList:[]};
@@ -24,12 +24,17 @@ app.controller('goodsController' ,function($scope,$controller,goodsService,uploa
 	}
 	
 	//查询实体 
-	$scope.findOne=function(id){				
+	$scope.findOne=function(){
+        var id = $location.search()['id'];
+        // alert(id)
+        if (id == null) {
+            return;
+        }
 		goodsService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;
 			}
-		);				
+		);
 	}
 	
 	//保存 

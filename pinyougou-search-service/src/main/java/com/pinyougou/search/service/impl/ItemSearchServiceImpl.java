fdd9ae09f6a4d922e61b51bf92117cb356561667
Nickylai <497000143@qq.com>
2019-05-05 23:38:49 +0800

    Day09 完成search服务层代码

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -1,8 +1,16 @@
 package com.pinyougou.search.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.ScoredPage;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,9 +23,19 @@ import java.util.Map;
 @Service(timeout = 3000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 
+    @Autowired
+    private SolrTemplate solrTemplate;
 
     @Override
     public Map<String, String> search(Map searchMap) {
-        return null;
+        Map map = new HashMap();
+        Query query = new SimpleQuery("*:*");
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+
+        map.put("rows", page.getContent());
+
+        return map;
     }
 }

e7de84f926939e5c67def5a96056e874334408e2
Nickylai <497000143@qq.com>
2019-05-06 20:35:42 +0800

    Day10前端增加查询条件

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,6 @@
 app.controller('searchController', function ($scope,searchService) {
+    //定义搜索对象的结构
+    $scope.searchMap = {'keywords': '', 'category': '','brand':'','spec':{}};
 
     //搜索
     $scope.search=function () {
@@ -7,6 +9,17 @@ app.controller('searchController', function ($scope,searchService) {
                 $scope.resultMap = response;
             }
         );
+    }
+
+    //添加搜索项 改变searchMap的值
+    $scope.addSearchItem=function (key,value) {
+        if (key == 'category' || key == 'brand') {
+            //如果用户点击的是品牌或分类
+            $scope.searchMap[key] = value;
+        } else {
+            //用户点击的是规格
+            $scope.searchMap.spec[key] = value;
+        }
     }
 
 });

e3b5f683712f923557f0a021e59529a472359b0d
Nickylai <497000143@qq.com>
2019-05-06 15:17:16 +0800

    Day10高亮显示搜索的关键字，后端search-service修改高亮方法，完成数据查询，返回给前端的数据带上高亮格式

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,12 +5,13 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,10 +30,45 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     @Override
     public Map<String, String> search(Map searchMap) {
         Map map = new HashMap();
-        Query query = new SimpleQuery("*:*");
+//        Query query = new SimpleQuery("*:*");
+//        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+//        query.addCriteria(criteria);
+//        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+//
+//        map.put("rows", page.getContent());
+
+        //高亮显示
+
+        HighlightQuery query=new SimpleHighlightQuery();
+        //高亮域
+        HighlightOptions highlightOptions=new HighlightOptions().addField("item_title");
+        //前缀后缀
+        highlightOptions.setSimplePrefix("<em style='color:red'>");
+        highlightOptions.setSimplePostfix("</em>");
+        //为查询对象设置高亮
+        query.setHighlightOptions(highlightOptions);
+
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
-        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+
+        //高亮页对象
+        HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+        //高亮集合入口
+        List<HighlightEntry<TbItem>> list = page.getHighlighted();
+        for (HighlightEntry<TbItem> entry : list) {
+            //获取高亮列（高亮域的数量）
+            List<HighlightEntry.Highlight> highlightList = entry.getHighlights();
+            /*
+            for (HighlightEntry.Highlight h : highlightList) {
+                //每个域可能有多个存储值
+                List<String> sns = h.getSnipplets();
+                System.out.println(sns);
+            }*/
+            if (highlightList.size()>0&&highlightList.get(0).getSnipplets().size()>0) {
+                TbItem item = entry.getEntity();
+                item.setTitle(highlightList.get(0).getSnipplets().get(0));
+            }
+        }
 
         map.put("rows", page.getContent());
 

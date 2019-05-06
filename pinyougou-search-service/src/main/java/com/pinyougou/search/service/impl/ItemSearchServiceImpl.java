c1626e8429efdfb171a11506b7661766f43f38ee
Nickylai <497000143@qq.com>
2019-05-06 18:44:06 +0800

    Day10后端分组查询代码，实现按品牌分类查询

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -4,12 +4,12 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.*;
-import org.springframework.data.solr.core.query.result.HighlightEntry;
-import org.springframework.data.solr.core.query.result.HighlightPage;
-import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.data.solr.core.query.result.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,15 +34,18 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 //        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
 //        query.addCriteria(criteria);
 //        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-//
 //        map.put("rows", page.getContent());
 
-        //查询列表
+        //1.查询列表
         map.putAll(searchList(searchMap));
-
+        //2.分组查询商品分类列表
+        map.put("categoryList",searchCategoryList(searchMap));
         return map;
     }
-    //查询列表
+    /**
+     * 查询列表
+     */
+
     private Map searchList(Map searchMap) {
         Map map = new HashMap();
         //高亮显示
@@ -81,5 +84,34 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 
     }
 
+    /**
+     * 分组查询
+     */
+    private List<String> searchCategoryList(Map searchMap) {
+        List<String> list = new ArrayList<>();
+        Query query=new SimpleQuery("*:*");
+        //关键字查询，相当于sql的where查询
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
 
+        //设置分组查询的选项
+        GroupOptions groupOptions=new GroupOptions().addGroupByField("item_category");
+        query.setGroupOptions(groupOptions);
+
+        //获取分组查询到的页面
+        GroupPage<TbItem> page = solrTemplate.queryForGroupPage(query, TbItem.class);
+        //获取分组结果
+        GroupResult<TbItem> groupResult = page.getGroupResult("item_category");
+        //获取分组入口页
+        Page<GroupEntry<TbItem>> groupEntries = groupResult.getGroupEntries();
+        //获取分组入口集合
+        List<GroupEntry<TbItem>> entryList = groupEntries.getContent();
+
+        for (GroupEntry<TbItem> entry : entryList) {
+            //将分页的结果添加到返回值
+            list.add(entry.getGroupValue());
+        }
+
+        return list;
+    }
 }
